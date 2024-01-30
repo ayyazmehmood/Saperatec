98db07707cde83e9919161bc875810c56a90f959
rajusaperatecverum <developer1-sap@saperatec.de>
2024-01-30 06:16:45 -0500

    test

--- ForecastWebApp/Pages/Index.cshtml.cs
+++ ForecastWebApp/Pages/Index.cshtml.cs
@@ -28,7 +28,7 @@ namespace ForecastWebApp.Pages
         protected void Forecast()
         {
             VerumSession verumSession = new VerumSession();
-            verumSession.Logon("ffriedrich@saperatec.de", "17Draconis$b", "Server=tcp:saperatec01.database.windows.net,1433;Initial Catalog=VerumTest;Persist Security Info=False;User ID=azureadmin;Password=U327Shns; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; ");
+            verumSession.Logon("ffriedrich@saperatec.de", "17Draconis$b", "Server=tcp:saperatec01.database.windows.net,1433;Initial Catalog=verum;Persist Security Info=False;User ID=azureadmin;Password=U327Shns; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30; ");
             Plan plan = new Plan();
             plan.SelectViaKey("Budget 2021");
             plan.ToForecast(null, null, true);
